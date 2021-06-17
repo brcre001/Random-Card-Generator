0b702a79686a3b193ec84fd121cf20dbdd895821
brcre001 <brandon.f.creed@gmail.com>
2021-06-17 19:18:02 +0000

    Made random card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["spade", "heart", "club", "diamond"];
+  let suitPicker = Math.floor(Math.random() * suits.length);
+  let cards = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let cardsPicker = Math.floor(Math.random() * cards.length);
+
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "green";
+
+  let classDiv = document.querySelector(".card");
+  classDiv.style.width = "225px";
+  classDiv.style.height = "350px";
+  classDiv.style.margin = "10px";
+  classDiv.style.textAlign = "center";
+  classDiv.style.verticalAlign = "middle";
+  classDiv.append(cards[cardsPicker]);
+  console.log(classDiv.target);
 };
